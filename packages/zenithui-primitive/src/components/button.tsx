45785dd32681ed2156cdfeea09557dd5e2c618bf
ChanduBobbili <chandubobbili12@gmail.com>
2025-01-20 11:53:34 +0530

    Feat: add CountdownTimer and Dialog components; refactor Button and utils

--- packages/zenithui-primitive/src/components/button.tsx
+++ packages/zenithui-primitive/src/components/button.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import { cn } from "../utils";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
@@ -8,7 +9,7 @@ const Button: FC<ButtonProps> = ({ children, className, ...rest }) => {
   return (
     <button
       {...rest}
-      className={`px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 ${className}`}
+      className={cn("px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600", className)}
     >
       {children}
     </button>
--- packages/zenithui-primitive/src/index.ts
+++ packages/zenithui-primitive/src/index.ts
@@ -1,2 +1,18 @@
 import "./index.css";
-export { default as Button } from "./button";
+// Import of Button Component
+export { default as Button } from "./components/button";
+//  Import of Dialog Component
+export {
+	Dialog,
+	DialogPortal,
+	DialogOverlay,
+	DialogTrigger,
+	DialogClose,
+	DialogContent,
+	DialogHeader,
+	DialogFooter,
+	DialogTitle,
+	DialogDescription,
+} from "./components/dialog";
+// Import of Count Down Component
+export { default as CountdownTimer } from "./components/count-down";
