cc42ae60a9b0fc4aa1bee74a21e03a235571dbef
ChanduBobbili <chandubobbili12@gmail.com>
2025-01-21 10:55:24 +0530

    style: lint fix

--- apps/react-test-app/eslint.config.js
+++ apps/react-test-app/eslint.config.js
@@ -8,43 +8,43 @@ import eslintPluginPrettier from "eslint-plugin-prettier"
 import tanstackQuery from "@tanstack/eslint-plugin-query"
 
 export default [
-	{
-		files: ["**/*.{ts,tsx,js,jsx}"],
-		languageOptions: {
-			ecmaVersion: 2020,
-			parser: tsParser,
-			globals: globals.browser,
-		},
-		plugins: {
-			"react-hooks": reactHooks,
-			"react-refresh": reactRefresh,
-			"@typescript-eslint": tseslint,
-			react,
-			prettier: eslintPluginPrettier,
-			"@tanstack/query": tanstackQuery,
-		},
-		rules: {
-			...(reactRefresh.configs ? reactRefresh.configs.recommended.rules : {}),
-			...tseslint.configs.recommended.rules,
-			...react.configs.recommended.rules,
-			"react/react-in-jsx-scope": "off", // Disable the need for React to be in scope with JSX
-			"react/prop-types": "off", // Disable prop-types rule for TypeScript
-			"react-refresh/only-export-components": "off", // Disable Fast Refresh rule
-			"@typescript-eslint/ban-ts-comment": "warn",
-			"@typescript-eslint/no-explicit-any": "off",
-			"prettier/prettier": "warn",
-			"react-refresh/only-export-components": [
-				"warn",
-				{ allowConstantExport: true },
-			],
-		},
-		settings: {
-			react: {
-				version: "detect",
-			},
-		},
-	},
-	{
-		ignores: ["node_modules/", "dist/", "build/", "src/components"],
-	},
+  {
+    files: ["**/*.{ts,tsx,js,jsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      parser: tsParser,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "@typescript-eslint": tseslint,
+      react,
+      prettier: eslintPluginPrettier,
+      "@tanstack/query": tanstackQuery,
+    },
+    rules: {
+      ...(reactRefresh.configs ? reactRefresh.configs.recommended.rules : {}),
+      ...tseslint.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off", // Disable the need for React to be in scope with JSX
+      "react/prop-types": "off", // Disable prop-types rule for TypeScript
+      "react-refresh/only-export-components": "off", // Disable Fast Refresh rule
+      "@typescript-eslint/ban-ts-comment": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
+      "prettier/prettier": "warn",
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    ignores: ["node_modules/", "dist/", "build/", "src/components"],
+  },
 ]
