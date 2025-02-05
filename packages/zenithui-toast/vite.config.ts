a5af73a26e792e03f2ac357e8ed191313cce9477
ChanduBobbili <chandubobbili12@gmail.com>
2025-02-05 22:49:46 +0530

    chore: update dependencies and remove unused Tailwind CSS configuration from zenithui-toast package

--- packages/zenithui-toast/vite.config.ts
+++ packages/zenithui-toast/vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import path from "path"
-import tailwindcss from "tailwindcss"
-import autoprefixer from "autoprefixer"
 import dts from "vite-plugin-dts"
-import { libInjectCss } from "vite-plugin-lib-inject-css"
 
 export default defineConfig({
   build: {
@@ -31,14 +28,5 @@ export default defineConfig({
     },
     sourcemap: true,
   },
-  plugins: [
-    react(),
-    libInjectCss(),
-    dts({ copyDtsFiles: true, rollupTypes: true }),
-  ],
-  css: {
-    postcss: {
-      plugins: [tailwindcss(), autoprefixer()],
-    },
-  },
+  plugins: [react(), dts({ copyDtsFiles: true, rollupTypes: true })],
 })
