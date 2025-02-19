70f19372ef786d12ca9652212a9dcb02f8ee0bb2
ChanduBobbili <chandubobbili12@gmail.com>
2025-02-19 19:02:31 +0530

    feat: document for time picker

--- apps/zenithui-docs/src/content/time-picker/_meta.js
+++ apps/zenithui-docs/src/content/time-picker/_meta.js
@@ -1,7 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  index: "",
+  "getting-started": "",
   "time-picker": "",
-  toast: "",
-  "day-picker": "",
+  "count-down": "",
 }
