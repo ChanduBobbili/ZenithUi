c3df476af9f957c4b2bac38ed352dcd1d7b7ba40
ChanduBobbili <chandubobbili12@gmail.com>
2025-03-25 13:40:57 +0530

    fix: update development port and refactor SVG handling in day-picker component

--- packages/@zenithui/day-picker/rslib.config.ts
+++ packages/@zenithui/day-picker/rslib.config.ts
@@ -1,17 +1,16 @@
 import { pluginReact } from "@rsbuild/plugin-react"
 import { defineConfig } from "@rslib/core"
-import { pluginSvgr } from "@rsbuild/plugin-svgr"
 import { pluginCssMinimizer } from "@rsbuild/plugin-css-minimizer"
 
 export default defineConfig({
   source: {
     entry: {
-      index: ["./src/index.tsx"],
+      index: ["./src/**"],
     },
   },
   lib: [
     {
-      bundle: true,
+      bundle: false,
       dts: true,
       format: "esm",
     },
@@ -29,5 +28,5 @@ export default defineConfig({
       "@": "./src",
     },
   },
-  plugins: [pluginReact(), pluginSvgr(), pluginCssMinimizer()],
+  plugins: [pluginReact(), pluginCssMinimizer()],
 })
